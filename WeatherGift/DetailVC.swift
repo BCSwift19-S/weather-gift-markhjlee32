4b37a2489a845f23790dbc93b7921238878ef6e6
Jason Tee <Jason@macbook-pro.local>
2019-03-06 04:26:39 -0500

    Completed 7.2

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by Jason Tee on 3/6/19.
+//  Copyright © 2019 Jason Tee. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
